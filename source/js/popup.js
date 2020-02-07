ab8bfd32f7a0ddebd8770f54c7b2aa6562794c63
Anastasia Videneeva <nastya.videneeva@gmail.com>
2020-02-07 10:00:41 -0500

    Adds focus on name field on popup open

--- source/js/popup.js
+++ source/js/popup.js
@@ -5,8 +5,15 @@ var popup = document.querySelector('.popup');
 popupOpenButton.addEventListener('click', function () {
   popup.classList.remove('popup--closed');
 
+  var popupNameInput = popup.querySelector('.popup-form__input--name');
+  // при открытии модального окна фокус в поле "Имя"
+  popupNameInput.focus();
+
   var closePopupButton = popup.querySelector('.popup-close');
   closePopupButton.addEventListener('click', function () {
     popup.classList.add('popup--closed');
   });
 });
+
+// TODO: закрытие модального окна по кнопке "Х" [x],  по нажатию клавиши Esc [], по клику на overlay[].
+// TODO: значения полей "Имя", "Телефон" и "Ваше сообщение" должны храниться в localStorage
