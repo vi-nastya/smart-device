b88b749121e3345d38b802c735c2c67dc47dc400
Anastasia Videneeva <nastya.videneeva@gmail.com>
2020-02-09 19:52:44 -0500

    Adds mask for modal and contact form phone field

--- source/js/main.js
+++ source/js/main.js
@@ -17,3 +17,27 @@ popupOpenButton.addEventListener('click', function () {
 
 // TODO: закрытие модального окна по кнопке "Х" [x],  по нажатию клавиши Esc [], по клику на overlay[].
 // TODO: значения полей "Имя", "Телефон" и "Ваше сообщение" должны храниться в localStorage
+
+
+// МАСКА ВВОДА
+var contactPhoneInput = document.getElementById('contact-phone');
+var modalPhoneInput = document.getElementById('modal-phone');
+
+var maskOptions = {
+  mask: '+7(000)000-00-00'
+};
+
+var contactMask = IMask(contactPhoneInput, maskOptions);
+contactPhoneInput.addEventListener('focus', function () {
+  if (contactMask.value === '') {
+    contactMask.value = '+7(';
+  }
+});
+
+var modalMask = IMask(modalPhoneInput, maskOptions);
+modalPhoneInput.addEventListener('focus', function () {
+  if (modalMask.value === '') {
+    modalMask.value = '+7(';
+  }
+});
+
